9f02e2c8d25a57607f5aced1fac03d91bd298e4a
Jean Trujillo <jeancarlostrujillo@gmail.com>
2022-01-04 15:47:40 -0400

    init Node, Mongodb, CRUD

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
 const colors = require('colors');
 const express = require('express');
 const app = express();
-
+app.use( express.json() );
 
 app.get('/', (req,res)=>{
     res.send('Hellow World');
+});
+
+function logger(req,res,next){
+    
+    console.log(` ${req.method}  ${req.path}`);
+}
+
+app.get('/user', (req,res)=>{
+    res.json({
+        id:1,
+        name:"jean",
+        lastname:"trujillo",
+        age:27
+    });
+});
+
+app.post('/user/:id', (req,res)=>{
+    console.log( req.body );
+    console.log( req.params );
+    res.send('Successfully user');
 });
 
 app.listen(3000, ()=>{
